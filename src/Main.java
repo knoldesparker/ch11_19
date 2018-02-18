c4f994f5ed8486213c68b9a39fc429d4f17fb9f7
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-02-18 10:51:50 +0100

    prints out a list of map values

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Map<String, Integer> mapMap = new HashMap<>();
+        mapMap.put("Mat", 6);
+        mapMap.put("Casper", 5);
+        mapMap.put("Marty", 5);
+        mapMap.put("G", 6);
+        mapMap.put("H", 6);
+
+        rarest(mapMap);
+
+    }
+
+    public static void rarest(Map<String, Integer> map) {
+        Map<Integer, Integer> rareMap = new TreeMap<>();
+        Iterator<String> itr = map.keySet().iterator();
+
+       while (itr.hasNext()){
+           int i = map.get(itr.next());
+           if (rareMap.containsKey(i)){
+               rareMap.put(i,rareMap.get(i)+1);
+           }else {
+               rareMap.put(i,1);
+           }
+       }
+        System.out.println(rareMap);
+
     }
 }
